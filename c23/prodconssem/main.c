21701c1749ad98f2cbe517e89c7928cc626aa8e5
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2025-03-28 12:03:59 +0100

    Minor syntax issues.

--- c23/prodconssem/main.c
+++ c23/prodconssem/main.c
@@ -5,16 +5,16 @@
 constexpr int buffer_size = 1024;
 constexpr int num_elements = 100'000;
 
-sem_t elements;
-sem_t holes;
+sem_t used_slots;
+sem_t empty_slots;
 int buffer[buffer_size];
 
 void * producer(void *);
 void * consumer(void *);
 
 int main(void) {
-  sem_init(&elements, 0, 0);
-  sem_init(&holes, 0, buffer_size);
+  sem_init(&used_slots, 0, 0); // Initially: no used slots
+  sem_init(&empty_slots, 0, buffer_size); // Initially: All empty slots
 
   pthread_t th_producer;
   pthread_create(&th_producer, nullptr, producer, nullptr);
@@ -24,29 +24,29 @@ int main(void) {
   pthread_join(th_producer, nullptr);
   pthread_join(th_consumer, nullptr);
 
-  sem_destroy(&elements);
-  sem_destroy(&holes);
+  sem_destroy(&used_slots);
+  sem_destroy(&empty_slots);
 }
 
 void * producer(void *) {
   int pos = 0;
-  for (int i = 0; i < num_elements; i++) {
+  for (int i = 0; i < num_elements; ++i) {
     int data = i;  // Generate data somehow
-    sem_wait(&holes);
+    sem_wait(&empty_slots);
     buffer[pos] = data;
     pos = (pos + 1) % buffer_size;
-    sem_post(&elements);
+    sem_post(&used_slots);
   }
   pthread_exit(nullptr);
 }
 
 void * consumer(void *) {
   int pos = 0;
-  for (int i = 0; i < num_elements; i++) {
-    sem_wait(&elements);
+  for (int i = 0; i < num_elements; ++i) {
+    sem_wait(&used_slots);
     int data = buffer[pos];
     pos = (pos + 1) % buffer_size;
-    sem_post(&holes);
+    sem_post(&empty_slots);
     printf("%d\n", data);
   }
   pthread_exit(nullptr);
