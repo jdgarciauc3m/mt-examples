ed484255b1061e3bc3ad1cd641d4e30b937a2773
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2025-04-01 18:59:57 +0200

    Use a semaphore.

--- cpp23/server/srvthr/main.cpp
+++ cpp23/server/srvthr/main.cpp
@@ -1,6 +1,6 @@
-#include <thread>
 #include <chrono>
 #include <print>
+#include <thread>
 
 // Server library
 #include "request.hpp"
@@ -10,32 +10,33 @@ int main() {
   using namespace std::chrono;
   auto start = std::chrono::high_resolution_clock::now();
 
-  auto service = [](server::request req) {
+  std::counting_semaphore sem_nchildren {0};
+  auto service = [&](server::request req) {
     std::println("Starting service for ID: {}", req.id());
     req.process();
     req.reply();
+    sem_nchildren.release();
     std::println("Ending service for ID: {}", req.id());
   };
 
-  auto receiver = [&] {
-    constexpr int max_requests = 5;
-    int num_in_service = 0;
 
-    std::vector<std::jthread> service_threads;
-    for (int i = 0; i < max_requests; i++) {
-      auto req = server::receive_request();
-      num_in_service++;
-      service_threads.emplace_back(service, req);
-    }
+  constexpr int max_requests = 5;
+  int num_in_service = 0;
 
-  };
-
-  std::jthread receiver_th{receiver};
-  receiver_th.join();
+  for (int i = 0; i < max_requests; i++) {
+    auto req = server::receive_request();
+    num_in_service++;
+    std::jthread service_th {service, req};
+    service_th.detach();
+  }
+  while (num_in_service > 0) {
+    std::println("Waiting for children");
+    sem_nchildren.acquire();
+    num_in_service--;
+    std::println("Child terminated");
+  }
 
   const auto stop = high_resolution_clock::now();
   const auto diff = duration_cast<seconds>(stop - start);
   std::println("Time: {}", diff.count());
 }
-
-
